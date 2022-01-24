41229910508953751c4acc139477f4b5f3455d67
Sahu2002 <84759422+utkarsh-iitbhu@users.noreply.github.com>
2022-01-25 01:34:05 +0530

    Update Python Exercises_test.py

--- Python Exercises_test.py
+++ Python Exercises_test.py
@@ -1,32 +1,33 @@
 import numpy as np
-import Python Exercises
-funcs = [power, split, format, domainGet, findDog, countDog, lambdafunc, caught_speeding]
+import Python_Exercises
+funcs = [Python_Exercises.power, Python_Exercises.split_str, Python_Exercises.format, Python_Exercises.domainGet,
+         Python_Exercises.findDog, Python_Exercises.countDog, Python_Exercises.lambdafunc, Python_Exercises.caught_speeding]
 debug = False
 test_cases = {
               'power' : [
                         [(7,4),2401]
                         ],
-              'split' : [
-                         ["Hi there Sam!",["Hi","there","Sam!"]]
+              'split_str' : [
+                         [("Hi there Sam!",),["Hi","there","Sam!"]]
               ],
               'format' : [
-                          [('Earth',12742),"The diameter of Earth is 12472 kilometers."]
+                          [("Earth",12742,),'The diameter of Earth is 12742 kilometers.']
               ],
 
               'domainGet' : [
-                              ['user@domain.com','user@domain.com'']
+                              [('user@domain.com',),'domain.com']
                                 ],
               'findDog' : [
-                              ['Is there a dog here?',True]
+                              [('Is there a dog here?',),True]
                                     ],
               'countDog' : [
-                            ['This dog runs faster than the other dog dude!',2]
+                            [('This dog runs faster than the other dog dude!',),2]
                             ],
-              'lambdafunc' : [ ['soup','dog','salad','cat','great'],
+              'lambdafunc' : [[ (['soup','dog','salad','cat','great'],),
                               ['soup','salad'],
-                              ],
+              ]],
               'caught_speeding' : [
-                                   [(81,True),Small Ticket]
+                                   [(81,True),"Small Ticket"]
               ]
              }     ## Will be filled after submission date
 
@@ -34,18 +35,31 @@ score = 0
 
 for func in funcs:
     flag = 1
-    for i,test_case in enumerate(test_cases[func._name_]):
+    for i,test_case in enumerate(test_cases[func.__name__]):
+        
         x, y = test_case
+
         try:
-            assert y==func(*x)
+            if func.__name__ == 'eqn_solver':
+                np.testing.assert_allclose( y, func(*x) )
+
+            else:
+                
+                assert y==func(*x)
+                
         except Exception as e:
-            flag = 1
-            print("function "+func._name_+" fails on testcase "+str(i+1),end="")
+            print(e)
+            flag = 0
+            print("function "+func.__name__+" fails on testcase "+str(i+1),end="")
             if debug:
                 print("because ")
-                print(e)
+                
             else:
                 print("")
+    if flag==1:
+        print("function "+func.__name__+" passes all the test cases!")
     score += flag
 
-print("Your score is "+str(score)+"!!, You will receive extra points for smart implementation directly in the pull request")
+print("Your score is "+str(score)+"!!,\nYou will receive extra points for smart implementation directly on the pull request")
+if score!=len(funcs):
+    raise Exception("Did not pass all tests")
