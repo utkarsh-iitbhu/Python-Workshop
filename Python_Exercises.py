0e0813d3901921a888e163751134808a04221440
Ameya <ameya.k.sp@gmail.com>
2022-01-30 22:50:32 +0530

    Completed Exercise

--- Python_Exercises.py
+++ Python_Exercises.py
@@ -4,11 +4,13 @@
 # Answer the questions or complete the tasks outlined in bold below.
 
 
+
 def power(a,b):
     
-    # ** What is 7 to the power of 4?**
-    
-    return None
+  # ** What is 7 to the power of 4?**
+  
+  return a**b
+
 
 
 
@@ -20,7 +22,9 @@ def split_str(s):
 #     
 # **into a list. **
 
-    return None
+  print(s.split())
+  return None
+
 
 
 def format(planet,diameter):
@@ -34,7 +38,10 @@ def format(planet,diameter):
 # 
 #     The diameter of Earth is 12742 kilometers.
 
-    return None
+  print("The diameter of {} is {} kilometers.".format(planet,diameter))
+  return None
+
+
 
 
 
@@ -43,8 +50,10 @@ def indexing(lst):
 # ** Given this nested list, use indexing to grab the word "hello" **
 
 #lst = [1,2,[3,4],[5,[100,200,['hello']],23,11],1,7]
+  print(lst[3][1][2][0])
+  return None
 
-    return None
+
 
 
 def dictionary(d):
@@ -52,17 +61,17 @@ def dictionary(d):
 # ** Given this nested dictionary grab the word "hello". Be prepared, this will be annoying/tricky **
 
 # d = {'k1':[1,2,3,{'tricky':['oh','man','inception',{'target':[1,2,3,'hello']}]}]}
-
-
-    return None
+  print(d["k1"][3]["tricky"][3]["target"][3])
+  return None
 
 
 def subjective():
     
 # ** What is the main difference between a tuple and a list? **
 # Tuple is _______
+  print("immutable")
+  return None
 
-    return None
 
 
 
@@ -74,22 +83,22 @@ def domainGet(email):
 #     user@domain.com
 #     
 # **So for example, passing "user@domain.com" would return: domain.com**
+  return email.split('@')[1]
 
-    return None
 
 
 def findDog(st):
     
 # ** Create a basic function that returns True if the word 'dog' is contained in the input string. Don't worry about edge cases like a punctuation being attached to the word dog, but do account for capitalization. **
-
-    return None
+  return st.find("dog") > -1
 
 
 def countDog(st):
 
 # ** Create a function that counts the number of times the word "dog" occurs in a string. Again ignore edge cases. **
 
-    return None
+  return st.count("dog")
+
 
 
 
@@ -103,7 +112,8 @@ def lambdafunc(seq):
 # 
 #     ['soup','salad']
 
-    return None
+  return list(filter(lambda a:a[0]=='s',seq))
+
 
 
 def caught_speeding(speed, is_birthday):
@@ -114,8 +124,20 @@ def caught_speeding(speed, is_birthday):
 #   If your speed is 60 or less, the result is "No Ticket". If speed is between 61 
 #   and 80 inclusive, the result is "Small Ticket". If speed is 81 or more, the result is "Big    Ticket". Unless it is your birthday (encoded as a boolean value in the parameters of the function) -- on your birthday, your speed can be 5 higher in all 
 #   cases. **
-
-    return None
+  if is_birthday ==True:
+    if speed <= 65:
+      return "No Ticket"
+    elif speed <=85:
+      return "Small ticket"
+    else :
+      return "Big Ticket"
+  else :
+    if speed <= 60:
+      return "No Ticket"
+    elif speed <=80:
+      return "Small ticket"
+    else :
+      return "Big Ticket"
 
 
 ## Numpy Exercises
@@ -128,9 +150,7 @@ def create_arr_of_fives():
   #### Create an array of 10 fives
   #### Convert your output into list 
   #### e.g return list(arr) 
-
-  return None
-
+  return list(np.ones(10) * 5)
 
 
 def even_num():
@@ -139,8 +159,7 @@ def even_num():
   ### Convert your output into list 
   ### e.g return list(arr) 
 
-  return None
-
+  return list(np.arange(10,51,2))
 
 
 def create_matrix():
@@ -148,8 +167,8 @@ def create_matrix():
   ### Create a 3x3 matrix with values ranging from 0 to 8
   ### Convert your output into list 
   ### e.g return (arr).tolist()
-
-  return None
+  arr_2d = np.absolute(np.random.randn(3,3)*8)
+  return arr_2d.tolist()
 
 
 
@@ -158,9 +177,9 @@ def linear_space():
   ### Create an array of 20 linearly spaced points between 0 and 1
   ### Convert your output into list 
   ### e.g return list(arr) 
+  arr=np.linspace(0,1,20)
 
-  return None
-
+  return list(arr)
 
 
 def decimal_mat():
@@ -168,8 +187,13 @@ def decimal_mat():
   ### Create an array of size 10*10 consisting of numbers from 0.01 to 1
   ### Convert your output into list 
   ### e.g return (arr).tolist()
+  arr=np.zeros((10,10))
+  for i in range(0,10):
+    for j in range(0,10):
+      arr[i][j]=(i*10 + j + 1)/100
+      
+  return arr.tolist()
 
-  return None
 
 
 
@@ -189,8 +213,8 @@ def slices_1():
   # array([[12, 13, 14, 15],
   #      [17, 18, 19, 20],
   #      [22, 23, 24, 25]])
-
-  return None
+  a= arr[2: , 1:]
+  return a.tolist()
 
 
 
@@ -210,11 +234,13 @@ def slices_2():
   # array([[ 2],
   #      [ 7],
   #      [12]])
+  a = arr[:3,1]
+  return a.tolist()
 
-  return None 
 
 
 
+import numpy as np
 def slices_3():
     
   # This is a given array
@@ -230,8 +256,7 @@ def slices_3():
   ### e.g return (arr).tolist()
   # array([[16, 17, 18, 19, 20],
   #      [21, 22, 23, 24, 25]])
-    
-  return None 
-
+  a = arr[3: ,:]    
+  return a.tolist()
 
 # Great job!
