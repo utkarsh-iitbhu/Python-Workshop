652ab570bfbf664cbe8256db0f10e32268005ebf
Abhigyan Kumar <abhigyan.kumar.cer21@itbhu.ac.in>
2022-02-02 01:13:13 +0530

    sol

--- Python_Exercises.py
+++ Python_Exercises.py
@@ -37,9 +37,9 @@ def indexing(lst):
 
 def dictionary(d):  
 # ** Given this nested dictionary grab the word "hello". Be prepared, this will be annoying/tricky **
+  d = {'k1':[1,2,3,{'tricky':['oh','man','inception',{'target':[1,2,3,'hello']}]}]}
   return print(d["k1"][3]["tricky"][3]["target"][3])
 
-# d = {'k1':[1,2,3,{'tricky':['oh','man','inception',{'target':[1,2,3,'hello']}]}]}
 # dictionary(d)
 #----------------------------------------------
 
