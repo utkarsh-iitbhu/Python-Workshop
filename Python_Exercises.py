f42125d1db0ac20c6581e61fa135ae2b76455b12
Abhigyan Kumar <abhigyan.kumar.cer21@itbhu.ac.in>
2022-02-02 01:18:29 +0530

    sol

--- Python_Exercises.py
+++ Python_Exercises.py
@@ -101,25 +101,26 @@ def caught_speeding(speed, is_birthday):
 #   If your speed is 60 or less, the result is "No Ticket". If speed is between 61 
 #   and 80 inclusive, the result is "Small Ticket". If speed is 81 or more, the result is "Big    Ticket". Unless it is your birthday (encoded as a boolean value in the parameters of the function) -- on your birthday, your speed can be 5 higher in all 
 #   cases. **
+  val=""
   if is_birthday==False:
 
     if speed<=60:
-      print("No Ticket")
+      val="No Ticket"
     elif speed<=80:
-      print("Small Ticket")
+      val="Small Ticket"
     else:
-      print("Big Ticket")
+      val="Big Ticket"
 
   else:
 
     if speed<=65:
-      print("No Ticket")
+      val="No Ticket"
     elif speed<=85:
-      print("Small Ticket")
+      val="Small Ticket"
     else:
-      print("Big Ticket")
+      val="Big Ticket"
 
-  return None
+  return val
 
 # caught_speeding(75,False)
 #------------------------------------------------------
