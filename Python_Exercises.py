3234d8506299e30724754f7ab5b2d304630e814b
shriya-tiwari <shriya.tiwari.cse21@itbhu.ac.in>
2022-02-02 01:18:41 +0530

    'Solved'

--- Python_Exercises.py
+++ Python_Exercises.py
@@ -8,22 +8,24 @@ def power(a,b):
     
     # ** What is 7 to the power of 4?**
     
-    return None
+    return a**b
 
 
 
 def split_str(s):
+    lst=s.split()
     
     # ** Split this string:**
 # 
 #     s = "Hi there Sam!"
 #     
 # **into a list. **
-
-    return None
+    
+    return lst
 
 
 def format(planet,diameter):
+
     
 # ** Given the variables:**
 # 
@@ -33,8 +35,8 @@ def format(planet,diameter):
 # ** Use .format() to print the following string: **
 # 
 #     The diameter of Earth is 12742 kilometers.
-
-    return None
+    s= "The diameter of {} is {} kilometers.".format(planet, diameter)
+    return s
 
 
 
@@ -44,7 +46,7 @@ def indexing(lst):
 
 #lst = [1,2,[3,4],[5,[100,200,['hello']],23,11],1,7]
 
-    return None
+    return lst[3][1][2][0]
 
 
 def dictionary(d):
@@ -54,15 +56,15 @@ def dictionary(d):
 # d = {'k1':[1,2,3,{'tricky':['oh','man','inception',{'target':[1,2,3,'hello']}]}]}
 
 
-    return None
+    return d['k1'][3]['tricky'][3]['target'][3]
 
 
 def subjective():
     
 # ** What is the main difference between a tuple and a list? **
 # Tuple is _______
-
-    return None
+    s="immutable"
+    return s
 
 
 
@@ -75,21 +77,24 @@ def domainGet(email):
 #     
 # **So for example, passing "user@domain.com" would return: domain.com**
 
-    return None
+    return email.split('@')[-1]
 
 
 def findDog(st):
     
 # ** Create a basic function that returns True if the word 'dog' is contained in the input string. Don't worry about edge cases like a punctuation being attached to the word dog, but do account for capitalization. **
-
-    return None
+  if 'dog' in st:
+    return "True"
 
 
 def countDog(st):
 
 # ** Create a function that counts the number of times the word "dog" occurs in a string. Again ignore edge cases. **
-
-    return None
+  a=0
+  for item in st.split():
+    if item=='dog':
+      a+=1
+  return a
 
 
 
@@ -103,7 +108,7 @@ def lambdafunc(seq):
 # 
 #     ['soup','salad']
 
-    return None
+    return list(filter(lambda word: word[0]=='s', seq))
 
 
 def caught_speeding(speed, is_birthday):
@@ -114,8 +119,22 @@ def caught_speeding(speed, is_birthday):
 #   If your speed is 60 or less, the result is "No Ticket". If speed is between 61 
 #   and 80 inclusive, the result is "Small Ticket". If speed is 81 or more, the result is "Big    Ticket". Unless it is your birthday (encoded as a boolean value in the parameters of the function) -- on your birthday, your speed can be 5 higher in all 
 #   cases. **
+    if is_birthday==1:
+      if speed<=65:
+        ticket="No ticket"
+      elif speed>65 and speed<86:
+        ticket="Small ticket"
+      else:
+        ticket="Big ticket"
+    else:
+      if speed<=60:
+        ticket="No ticket"
+      elif speed>60 and speed<81:
+        ticket="Small ticket"
+      else:
+        ticket="Big ticket"
 
-    return None
+    return ticket
 
 
 ## Numpy Exercises
@@ -128,8 +147,8 @@ def create_arr_of_fives():
   #### Create an array of 10 fives
   #### Convert your output into list 
   #### e.g return list(arr) 
-
-  return None
+  ar=np.ones(10)*5
+  return list(ar)
 
 
 
@@ -138,8 +157,8 @@ def even_num():
   ### Create an array of all the even integers from 10 to 50
   ### Convert your output into list 
   ### e.g return list(arr) 
-
-  return None
+  ar=np.arange(10,51,2)
+  return list(ar)
 
 
 
@@ -148,8 +167,8 @@ def create_matrix():
   ### Create a 3x3 matrix with values ranging from 0 to 8
   ### Convert your output into list 
   ### e.g return (arr).tolist()
-
-  return None
+  ar=np.arange(0,9).reshape(3,3)
+  return ar.tolist()
 
 
 
@@ -158,8 +177,8 @@ def linear_space():
   ### Create an array of 20 linearly spaced points between 0 and 1
   ### Convert your output into list 
   ### e.g return list(arr) 
-
-  return None
+  ar=np.linspace(0,1,20)
+  return list(ar)
 
 
 
@@ -168,8 +187,8 @@ def decimal_mat():
   ### Create an array of size 10*10 consisting of numbers from 0.01 to 1
   ### Convert your output into list 
   ### e.g return (arr).tolist()
-
-  return None
+  ar=np.linspace(0.01,1,100).reshape(10,10)
+  return ar.tolist()
 
 
 
@@ -189,8 +208,8 @@ def slices_1():
   # array([[12, 13, 14, 15],
   #      [17, 18, 19, 20],
   #      [22, 23, 24, 25]])
-
-  return None
+  ar=arr[2: , 1:]
+  return ar.tolist()
 
 
 
@@ -211,7 +230,7 @@ def slices_2():
   #      [ 7],
   #      [12]])
 
-  return None 
+  return arr[:3,1:2].tolist()
 
 
 
@@ -231,7 +250,7 @@ def slices_3():
   # array([[16, 17, 18, 19, 20],
   #      [21, 22, 23, 24, 25]])
     
-  return None 
+  return arr[3:,:].tolist()
 
 
 # Great job!
