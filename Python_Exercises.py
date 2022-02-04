8c0ef11bef2ba138cc2cf8b0484ad844a83e4a7d
Anjali_ <95526372+anjali-42@users.noreply.github.com>
2022-02-05 03:00:29 +0530

    solutions final

--- Python_Exercises.py
+++ Python_Exercises.py
@@ -17,10 +17,10 @@ def split_str(s):
   return list(s.split())
 
 def format(planet,diameter):
-    return ( 'The diameter of {} is {} kilometers.' .format(planet,diameter)
+  return ( 'The diameter of {} is {} kilometers.' .format(planet,diameter)
 
 def indexing(lst):
-    return lst[3][1][2]
+  return lst[3][1][2]
 
 d = {'k1':[1,2,3,{'tricky':['oh','man','inception',{'target':[1,2,3,'hello']}]}]}
 def dictionary(d):
