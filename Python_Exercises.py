45a1c01f573b127fe886955e940aac71ec1d4ca8
Anjali_ <95526372+anjali-42@users.noreply.github.com>
2022-02-05 03:22:59 +0530

    Update Python_Exercises.py

--- Python_Exercises.py
+++ Python_Exercises.py
@@ -20,7 +20,13 @@ def format(planet,diameter):
   return ( 'The diameter of {} is {} kilometers.' .format(planet,diameter)
 
 def indexing(lst):
-    return lst[3][1][2]
+    
+# ** Given this nested list, use indexing to grab the word "hello" **
+
+#lst = [1,2,[3,4],[5,[100,200,['hello']],23,11],1,7]
+
+
+    return lst[3][1][2][0]
 
 d = {'k1':[1,2,3,{'tricky':['oh','man','inception',{'target':[1,2,3,'hello']}]}]}
 def dictionary(d):
