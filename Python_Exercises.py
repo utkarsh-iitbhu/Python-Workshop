f9b068447c5d0eef5f81af18dc03a3df34f48cf2
AnkitV-Git <96656348+AnkitV-Git@users.noreply.github.com>
2022-02-05 18:19:33 +0530

    Update Python_Exercises.py

--- Python_Exercises.py
+++ Python_Exercises.py
@@ -44,7 +44,7 @@ def indexing(lst):
 # ** Given this nested list, use indexing to grab the word "hello" **
 
 #lst = [1,2,[3,4],[5,[100,200,['hello']],23,11],1,7]
-    lst[3][1][2][0]
+  return  lst[3][1][2][0]
    
 
 
@@ -54,8 +54,8 @@ def dictionary(d):
 
 # d = {'k1':[1,2,3,{'tricky':['oh','man','inception',{'target':[1,2,3,'hello']}]}]}
 
-      d[k1][3][tricky][3][target][3]
-    return None
+  return    d['k1'][3]['tricky'][3]['target'][3]
+    
 
 
 def subjective():
