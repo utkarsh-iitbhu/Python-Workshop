bd12a64ec6dda432fdb17115dd57910e02245dca
Clover-Codes <95234232+Clover-Codes@users.noreply.github.com>
2022-02-06 12:52:06 +0530

    completed python exercises

--- Python_Exercises.py
+++ Python_Exercises.py
@@ -8,7 +8,7 @@ def power(a,b):
     
     # ** What is 7 to the power of 4?**
     
-    return None
+    return 7**4
 
 
 
@@ -20,7 +20,7 @@ def split_str(s):
 #     
 # **into a list. **
 
-    return None
+    return s.split();
 
 
 def format(planet,diameter):
@@ -34,7 +34,8 @@ def format(planet,diameter):
 # 
 #     The diameter of Earth is 12742 kilometers.
 
-    return None
+    txt = "The Diameter of {pl} is {di} kilometers."
+    return txt.format(pl=planet, di=diameter)
 
 
 
@@ -44,7 +45,7 @@ def indexing(lst):
 
 #lst = [1,2,[3,4],[5,[100,200,['hello']],23,11],1,7]
 
-    return None
+    return lst[3][1][2]
 
 
 def dictionary(d):
@@ -54,7 +55,7 @@ def dictionary(d):
 # d = {'k1':[1,2,3,{'tricky':['oh','man','inception',{'target':[1,2,3,'hello']}]}]}
 
 
-    return None
+    return d['k1'][3]['tricky'][3]['target'][3]
 
 
 def subjective():
@@ -62,7 +63,7 @@ def subjective():
 # ** What is the main difference between a tuple and a list? **
 # Tuple is _______
 
-    return None
+    return 'immutable'
 
 
 
@@ -75,21 +76,21 @@ def domainGet(email):
 #     
 # **So for example, passing "user@domain.com" would return: domain.com**
 
-    return None
+    return email[email.find('@')+1:]
 
 
 def findDog(st):
     
 # ** Create a basic function that returns True if the word 'dog' is contained in the input string. Don't worry about edge cases like a punctuation being attached to the word dog, but do account for capitalization. **
 
-    return None
+    return st.lower().__contains__('dog')
 
 
 def countDog(st):
 
 # ** Create a function that counts the number of times the word "dog" occurs in a string. Again ignore edge cases. **
 
-    return None
+    return st.lower().count("dog")
 
 
 
@@ -103,7 +104,7 @@ def lambdafunc(seq):
 # 
 #     ['soup','salad']
 
-    return None
+    return list(filter(lambda ele: ele[0]=='s', seq))
 
 
 def caught_speeding(speed, is_birthday):
@@ -114,8 +115,15 @@ def caught_speeding(speed, is_birthday):
 #   If your speed is 60 or less, the result is "No Ticket". If speed is between 61 
 #   and 80 inclusive, the result is "Small Ticket". If speed is 81 or more, the result is "Big    Ticket". Unless it is your birthday (encoded as a boolean value in the parameters of the function) -- on your birthday, your speed can be 5 higher in all 
 #   cases. **
+  if(is_birthday):
+    speed-=5
 
-    return None
+  if(speed<=60):
+    return 'No ticket'
+  elif(speed<=80):
+    return 'Small Ticket'
+  else:
+    return 'Big Ticket'
 
 
 ## Numpy Exercises
@@ -129,7 +137,7 @@ def create_arr_of_fives():
   #### Convert your output into list 
   #### e.g return list(arr) 
 
-  return None
+  return list(np.full((10), 5))
 
 
 
@@ -139,7 +147,7 @@ def even_num():
   ### Convert your output into list 
   ### e.g return list(arr) 
 
-  return None
+  return list(np.arange(10, 51, 2))
 
 
 
@@ -149,7 +157,7 @@ def create_matrix():
   ### Convert your output into list 
   ### e.g return (arr).tolist()
 
-  return None
+  return np.arange(0, 9, 1).reshape(3, 3).tolist()
 
 
 
@@ -159,7 +167,7 @@ def linear_space():
   ### Convert your output into list 
   ### e.g return list(arr) 
 
-  return None
+  return list(np.linspace(0, 1, 20))
 
 
 
@@ -169,7 +177,7 @@ def decimal_mat():
   ### Convert your output into list 
   ### e.g return (arr).tolist()
 
-  return None
+  return np.linspace(0.01, 1, 100).reshape(10, 10).tolist()
 
 
 
@@ -190,7 +198,7 @@ def slices_1():
   #      [17, 18, 19, 20],
   #      [22, 23, 24, 25]])
 
-  return None
+  return arr[2:, 1:].tolist()
 
 
 
@@ -211,7 +219,7 @@ def slices_2():
   #      [ 7],
   #      [12]])
 
-  return None 
+  return arr[:3, 1:2].tolist()
 
 
 
@@ -231,7 +239,7 @@ def slices_3():
   # array([[16, 17, 18, 19, 20],
   #      [21, 22, 23, 24, 25]])
     
-  return None 
+  return arr[3:].tolist()
 
 
 # Great job!
