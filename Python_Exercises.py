5ffe5bccda741355cc2ae79fe0caee6489cf36dd
Anjali_ <95526372+anjali-42@users.noreply.github.com>
2022-02-07 17:02:05 +0530

    new changes

--- Python_Exercises.py
+++ Python_Exercises.py
@@ -18,15 +18,12 @@ def split_str(s):
 
 def format(planet,diameter):
   return ( 'The diameter of {} is {} kilometers.' .format(planet,diameter)
-
-def indexing(lst):
-    
-# ** Given this nested list, use indexing to grab the word "hello" **
-
-#lst = [1,2,[3,4],[5,[100,200,['hello']],23,11],1,7]
-
-    return None
+          
+          
+lst = [1,2,[3,4],[5,[100,200,['hello']],23,11],1,7]
   
+def indexing(lst):
+
   return lst[3][1][2][0]
 
 d = {'k1':[1,2,3,{'tricky':['oh','man','inception',{'target':[1,2,3,'hello']}]}]}
